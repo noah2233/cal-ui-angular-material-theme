33aa1420c7df89da575203ab4fb6dff8aaabbae8
Noah Lubko <nlubko@gmail.com>
2020-08-08 13:02:45 +0300

    add buttons

--- cal-ui-mat-theme-sandbox/src/app/app.module.ts
+++ cal-ui-mat-theme-sandbox/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Material } from './material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, Material],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ cal-ui-mat-theme-sandbox/src/app/material.ts
@@ -0,0 +1,5 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+export const Material = [MatButtonModule, MatDividerModule, MatIconModule];
